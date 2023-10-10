8cf9f35cd359b941f76a5f7cbda143aa1fc04f46
pooh-14 <poojavetal14@gmail.com>
2023-10-10 22:08:07 +0530

    done

--- ss/src/Components/Home/Navbar.jsx
+++ ss/src/Components/Home/Navbar.jsx
@@ -2,13 +2,53 @@
 import { useContext, useEffect, useState } from 'react';
 import './Navbar.css'
 import { useNavigate } from 'react-router-dom'
+import { toast } from "react-hot-toast";
 // import { AuthContext } from '../../Context/AuthContext';
+import '../LogReg/Login.css'
 
 const Navbar = () => {
 
+  const [userData, setUserData] = useState({ email: "", password: "" });
   const [user, setUser] = useState({});
   const router = useNavigate();
   // const { state } = useContext(AuthContext);
+
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleLoginForm = () => {
+    setIsActive(!isActive);
+  };
+
+ 
+
+   
+  
+    const handleChange = (event) => {
+      setUserData({ ...userData, [event.target.name]: event.target.value });
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (userData.email && userData.password) {
+          var flag = false;
+          const allUsers = JSON.parse(localStorage.getItem("Users"));
+          for (var i = 0; i < allUsers.length; i++) {
+              if (allUsers[i].email == userData.email && allUsers[i].password == userData.password) {
+                  localStorage.setItem("Current-user", JSON.stringify(allUsers[i]))
+                  setUserData({ email: "", password: "" })
+                  toast.success("Login Successfull.")
+                  router('/')
+                  flag = true;
+                  break;
+              }
+          }
+          if (flag == false) {
+              toast.error("Please Check your email & password.")
+          }
+
+      } else {
+          toast.error("Please fill the all fields.")
+      }}
 
   // useEffect(() => {
   //   if (state) {
@@ -17,7 +57,8 @@ const Navbar = () => {
   // }, [state]);
 
   return (
-    <div className='btmbrdr'>
+    <div>
+      <div className='btmbrdr'>
       {/* <div class="loadingio-spinner-bars-qusjja94299"><div class="ldio-5m4rxymmg8a">
 <div></div><div></div><div></div><div></div>
 </div></div> */}
@@ -38,7 +79,7 @@ const Navbar = () => {
           <div>
           <i class="fa-regular fa-heart fa-lg"></i>
           <i onClick={()=>router('/cart')} class="fa-solid fa-bag-shopping fa-lg"></i>
-          <i onClick={() => router('/register')} class="fa-regular fa-circle-user fa-lg"></i>
+          <i onClick={toggleLoginForm} class="fa-regular fa-circle-user fa-lg"></i>
           </div>
         </div>
       </div>
@@ -60,6 +101,32 @@ const Navbar = () => {
       </div>
       
 
+    </div>
+    {/* -------------------------------****----------------------------- */}
+
+    <div id='login' className={`login-form ${isActive ? 'active' : ''}`}>
+        <div>
+        <h1>Log in </h1>
+        <p>for tailored experience</p>
+        </div>
+        <div id='forms2'>
+         <form onSubmit={handleSubmit}>
+            <input onChange={handleChange} type='email' name='email' placeholder='Enter Email ID*'/><br/>
+            <input onChange={handleChange} type='password' name='password' placeholder='Enter Password*'/><br/>
+            <button>Login</button>
+         </form>
+        </div>
+        <div>
+            <p>Login using</p>
+            <div>
+            <i class="fa-brands fa-facebook-f fa-xl"></i>
+            <i class="fa-brands fa-google fa-xl"></i>
+            </div>
+        </div>
+        <div>
+            <p>New User? <b onClick={() => router('/register')}> Register </b></p>
+            </div>
+    </div>
     </div>
   )
 }
--- ss/src/Components/LogReg/Login.jsx
+++ ss/src/Components/LogReg/Login.jsx
@@ -1,75 +1,53 @@
-import React, { useContext, useEffect, useState } from 'react'
-import './Login.css'
-import { useNavigate } from 'react-router-dom';
-// import { AuthContext } from '../../Context/AuthContext';
-import { toast } from "react-hot-toast";
-import axios from 'axios';
+// import React, { useContext, useEffect, useState } from 'react'
+// import './Login.css'
+// import { useNavigate } from 'react-router-dom';
+// // import { AuthContext } from '../../Context/AuthContext';
+// import { toast } from "react-hot-toast";
+// import axios from 'axios';
 
-const Login = () => {
-    const [userData, setUserData] = useState({ email: "", password: "" });
+// const Login = () => {
+//     const [userData, setUserData] = useState({ email: "", password: "" });
 
-    // const { state, dispatch } = useContext(AuthContext);
-    const router = useNavigate();
+//     // const { state, dispatch } = useContext(AuthContext);
+//     const router = useNavigate();
   
-    const handleChange = (event) => {
-      setUserData({ ...userData, [event.target.name]: event.target.value });
-    };
+//     const handleChange = (event) => {
+//       setUserData({ ...userData, [event.target.name]: event.target.value });
+//     };
   
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      if (userData.email && userData.password) {
-          var flag = false;
-          const allUsers = JSON.parse(localStorage.getItem("Users"));
-          for (var i = 0; i < allUsers.length; i++) {
-              if (allUsers[i].email == userData.email && allUsers[i].password == userData.password) {
-                  localStorage.setItem("Current-user", JSON.stringify(allUsers[i]))
-                  setUserData({ email: "", password: "" })
-                  toast.success("Login Successfull.")
-                  router('/')
-                  flag = true;
-                  break;
-              }
-          }
-          if (flag == false) {
-              toast.error("Please Check your email & password.")
-          }
+//     const handleSubmit = (event) => {
+//       event.preventDefault();
+//       if (userData.email && userData.password) {
+//           var flag = false;
+//           const allUsers = JSON.parse(localStorage.getItem("Users"));
+//           for (var i = 0; i < allUsers.length; i++) {
+//               if (allUsers[i].email == userData.email && allUsers[i].password == userData.password) {
+//                   localStorage.setItem("Current-user", JSON.stringify(allUsers[i]))
+//                   setUserData({ email: "", password: "" })
+//                   toast.success("Login Successfull.")
+//                   router('/')
+//                   flag = true;
+//                   break;
+//               }
+//           }
+//           if (flag == false) {
+//               toast.error("Please Check your email & password.")
+//           }
 
-      } else {
-          toast.error("Please fill the all fields.")
-      }}
-    // console.log(userData, "userData")
+//       } else {
+//           toast.error("Please fill the all fields.")
+//       }}
+//     // console.log(userData, "userData")
   
-    // useEffect(() => {
-    //   if (state?.user?.name) {
-    //     router("/");
-    //   }
-    // }, [state]);
+//     // useEffect(() => {
+//     //   if (state?.user?.name) {
+//     //     router("/");
+//     //   }
+//     // }, [state]);
 
-  return (
-    <div id='login'>
-        <div>
-        <h1>Log in </h1>
-        <p>for tailored experience</p>
-        </div>
-        <div id='forms2'>
-         <form onSubmit={handleSubmit}>
-            <input onChange={handleChange} type='email' name='email' placeholder='Enter Email ID*'/><br/>
-            <input onChange={handleChange} type='password' name='password' placeholder='Enter Password*'/><br/>
-            <button>Login</button>
-         </form>
-        </div>
-        <div>
-            <p>Login using</p>
-            <div>
-            <i class="fa-brands fa-facebook-f fa-xl"></i>
-            <i class="fa-brands fa-google fa-xl"></i>
-            </div>
-        </div>
-        <div>
-            <p>New User? <b onClick={() => router('/register')}> Register </b></p>
-            </div>
-    </div>
-  )
-}
+//   return (
+    
+//   )
+// }
 
-export default Login
+// export default Login
